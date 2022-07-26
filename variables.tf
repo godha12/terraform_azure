41dfcba982841471bfe0c6205be5b817c267d807
Godhadevi <godhadevi.tedla@intimetec.com>
2022-07-26 12:11:26 +0530

    modified files

--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "key_name" {
-  type        = string
-  default = "ecs_cluster_us-east-1.pem"
-  description = "The name for ssh key, used for aws_launch_configuration"
-}
 
 variable "cluster_name" {
   type        = string
